02cd408159bae7cb731ec44a29d744554033b723
vivian <vivian.v18723@gmail.com>
2022-07-11 17:49:30 +0100

    add reduce function

--- functional.js
+++ functional.js
@@ -5,23 +5,34 @@
 
 // map -> do same thing to every element & return result
 // filter -> only keep each element if passes test
+// reduce -> combine elements together and return the final result
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const result = arr
+// const result = arr
+//   .map((singleItem) => {
+//     return singleItem * 2;
+//   })
+//   .map((singleItem) => {
+//     return singleItem - 1;
+//   });
+// console.log(result1);
+
+// const result2 = arr
+//   .map((singleItem) => {
+//     return singleItem * 2;
+//   })
+//   .filter((singleItem) => {
+//     return singleItem > 8;
+//   });
+// console.log(result2);
+
+const result3 = arr
   .map((singleItem) => {
     return singleItem * 2;
   })
-  .map((singleItem) => {
-    return singleItem - 1;
+  .reduce((first, second) => {
+    return first + second;
   });
 
-const result2 = arr
-  .map((singleItem) => {
-    return singleItem * 2;
-  })
-  .filter((singleItem) => {
-    return singleItem > 8;
-  });
-
-console.log(result2);
+console.log(result3);
